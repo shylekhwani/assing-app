243b5d30b15f5a429b1367e5d13e2f3f85bcbb24
yash lekhwani~ <yashlekhwani49@gmail.com>
2025-04-05 00:43:43 +0530

    lint issue

--- app/components/HabitProgress/ProgressBar.tsx
+++ app/components/HabitProgress/ProgressBar.tsx
@@ -1,16 +1,21 @@
 interface ProgressBarProps {
-  percentage : number;
-};
-
-export const ProgressBar = function({percentage} : ProgressBarProps) {
+    percentage: number;
+  }
+  
+  export const ProgressBar = function({ percentage }: ProgressBarProps) {
+    const style = {
+      "--value": percentage,
+    } as React.CSSProperties;
+  
     return (
-        <div 
-            className="radial-progress text-white" 
-            style={{ "--value": percentage }} 
-            aria-valuenow={percentage} 
-            role="progressbar"
-        >
-            {Math.round(percentage)}%
-        </div>
+      <div
+        className="radial-progress text-white"
+        style={style}
+        aria-valuenow={percentage}
+        role="progressbar"
+      >
+        {Math.round(percentage)}%
+      </div>
     );
-};
+  };
+  
